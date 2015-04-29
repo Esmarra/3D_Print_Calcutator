581d67278053aee11f714ed3ea1dc271d12a4427
Esmarra <miguel.maranha@gmail.com>
2015-04-29 12:35:36 +0100

    Code

--- Print Calc Esm.cpp
+++ Print Calc Esm.cpp
@@ -14,35 +14,42 @@ Special Thanks:
 =========================================*/
 
 #include <iostream>
-#include <iomanip> // Using setprecision
+#include <iomanip> // Using set precision
 #include <cstdlib> //Using System
 
 using namespace std;
 
-char Exit='n';
-char go='n';
+//=== YOUR VALUES & RATES ===
+float machine_payrate = 2.5 ; 					    // Machine Cost per Hour
+float human_payrate = 10 ;							// Human Cost per Hour
+float profit_margin = 135 ;							// Profit Margin [Percentage]
+//=== ######### ######### ===
+
+
+//=== Calculator input Values ===
+float print_time = 0 ; 								// Time in minutes from p.hour and p.minute	[Minutes]
+float setup_time = 0 ;								// First Layer / Extruder Heating time 	    [Minutes]
+float print_length = 0;                             // Length of the print, after slicing       [Millimeters]
+float print_mass = 0 ;								// Print Mass		                        [Grams]
+float print_cost = 0 ;                              // Total Cost of the Print                  [Euro]
+
+float babysitting_time = 5 ; 						// Required attention time to print 	    [Minutes]
+float nonprinted_partscost = 0 ;					// Non Printed Parts Cost example: Metal Nut Inside print		[Euro]
+//=== #########     ######### ===
+
+//=== Other Common Variables ===
+int debug = 0;                                      // Set to 1 to get debug feedback
+int print_minute = 0 ;                              // Defines print time in minutes
+int print_hour = 0 ;                                // Defines print time in minutes
+float print_second = 0 ;                            // Defines print time in minutes
+char Exit='n';                                      // End Program
+char go='n';                                        // Start Program
+//=== #########    ######### ===
+
+//=== Functions ===
 void run();
 float calculate_print_time(int &print_hour,int &print_minute, float &print_second);
-int debug = 0;
-
-// Variables
-
-int print_minute = 0 ;
-int print_hour = 0 ;
-float print_time = 0 ; 								// Time in minutes from p.hour and p.minute	[Minutes]
-float print_second = 0 ;
-
-float machine_payrate = 2.5 ; 					    // Machine Cost per Hour
-float babysitting_time = 5 ; 						// Required attention time to print 	[Minutes]
-float human_payrate = 10 ;							// Human Cost per Hour
-float setup_time = 0 ;								// First Layer / Extruder Heating time 	[Minutes]
-float print_mass = 0 ;								// Print Mass		[Grams]
-float plastic_price = 34.56 ;							// Plastic Price per Kg 	[Euro]
-float nonprinted_partscost = 0 ;					// Non Printed Parts Cost example: Metal Nut Inside print		[Euro]
-float profit_margin = 135 ;							// Profit Margin [Percentage]																			#[ REV ]#
-
-float print_cost = 0 ;
-float length = 0;
+//=== ######### ===
 
 struct plastic {
     string name ;
@@ -52,15 +59,13 @@ struct plastic {
     float area;
 };
 
-
-
 int main(){
-
 
     while(Exit=='n'){
         system("cls");
         go='n';
-        run();
+        run();                                      // Main Program is Here
+
         cout<<"\n \t \t   Do you whish to quit the program ? (y/n): ";
         cin>>Exit;
     }
@@ -77,16 +82,10 @@ void run(){
     PLA.kg_cost=34.56;
     PLA.area=PLA.diameter/2 * PLA.diameter/2 * 3.1415926535897932384626433832795 ;
 
-    /*calculate area
-    float radius = diameter/2;
-    float area = radius * radius * 3.1415926535897932384626433832795;
-    float weight = (area * length * density);
-    */
     //RESETS
     print_time = 0;
-    print_minute = 0;
     setup_time = 0 ;
-    length =0 ;
+    print_length =0 ;
     print_mass = 0 ;
     while ( print_time == 0){
         cout <<"\n Type print time: "<<endl;
@@ -96,8 +95,9 @@ void run(){
         cin >> print_minute;
         cout <<"\t Seconds: ";
         cin >> print_second;
+        print_time = calculate_print_time(print_hour,print_minute,print_second);
     }
-    print_time = calculate_print_time(print_hour,print_minute,print_second);
+
     if( debug == 1) cout << "print time: "<< print_time <<" min."<<endl;
 
     while ( setup_time == 0 || setup_time >= print_time){
@@ -107,20 +107,20 @@ void run(){
 
     if (debug ==1 )cout << "Setup time(min): "<< setup_time <<endl;
 
-    while ( length ==0 ){
+    while ( print_length ==0 ){
         cout << "\n Type Total Print Length(mm): ";
-        cin >> length;
+        cin >> print_length;
     }
 
-    if (debug ==1 )cout << "Length(mm): "<< length <<endl;
+    if (debug ==1 )cout << "Length(mm): "<< print_length <<endl;
 
 
-    print_mass = PLA.area * length * PLA.density;
+    print_mass = PLA.area * print_length * PLA.density;
 
 
     print_cost = ((print_time * (machine_payrate / 60)) +
                     ((print_time / 100 * babysitting_time) * (human_payrate / 60 / 10)) +
-                        (setup_time * (human_payrate / 60)) + (print_mass * (plastic_price / 1000)) +
+                        (setup_time * (human_payrate / 60)) + (print_mass * (PLA.kg_cost / 1000)) +
                             nonprinted_partscost) / 100 * (100 + profit_margin);
 
 	cout<<"\n Total Printing Cost : "<<print_cost<<endl;
